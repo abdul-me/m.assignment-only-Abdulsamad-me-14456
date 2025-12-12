98925cc3a21707f1e7eb5dd4ce86029df63b923f
abdul-me <abdulsamad14456@gmail.com>
2025-12-12 13:08:47 +0500

    footer, header, navbars inlude like wishlist, notificatons, advanced search

---
+++ includes/navbar.php
@@ -0,0 +1,8 @@
+<nav>
+    <a href="index.php">Home</a>
+    <a href="books.php">Books</a>
+    <a href="about.php">About</a>
+    <a href="contact.php">Contact</a>
+    <a href="login.php">Login</a>
+    <a href="register.php">Register</a>
+</nav>
